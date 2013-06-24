7db3aa3a641d8c61ff04a90bd6ba749b004b9d28
Seth Karim Luis Martinez <guepardo190889@gmail.com>
2013-06-24 00:19:17 -0500

    Ya se logro la conexion a la base de datos

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,34 +1,41 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    //driverClassName = "org.h2.Driver"
+    //username = "sa"
+    //password = ""
+	
+	driverClassName = "org.postgresql.Driver"
+	username = "minitales"
+	password = "aeiou123"
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+	cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+	//cache.provider_class='org.hibernate.cache.EhCacheProvider'
 }
 // environment specific settings
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			dbCreate = "update" // one of 'create', 'create-drop','update'
+			url = "jdbc:postgresql://localhost:5432/minitales"
+            //dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
     test {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            //dbCreate = "update"
+            //url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
         }
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
+            //dbCreate = "update"
+            //url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            //pooled = true
+            /*properties {
                maxActive = -1
                minEvictableIdleTimeMillis=1800000
                timeBetweenEvictionRunsMillis=1800000
@@ -37,7 +44,7 @@ environments {
                testWhileIdle=true
                testOnReturn=true
                validationQuery="SELECT 1"
-            }
+            }*/
         }
     }
 }
---
+++ grails-app/domain/minitales/Usuario.groovy
@@ -0,0 +1,13 @@
+package minitales
+
+class Usuario {
+	
+	String primerNombre
+	String segundoNombre
+	Date fechaNacimiento
+	String password
+	
+
+    static constraints = {
+    }
+}
